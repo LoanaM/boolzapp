57ed8a430a63d4ea99f8c17b719dee82517e4386
LoanaWitch <loanawitch@live.com>
2018-04-12 12:29:53 +0200

    keypress correct

--- boolzappmain.js
+++ boolzappmain.js
@@ -6,12 +6,12 @@ $(document).ready(function(){
       var ora = new Date();
 
       if (NewMessage) {
-        $('.rightcontainer').append(
+        $('.messagescontainer').append(
           '<div class="messages rightgreen">' + NewMessage + '<span class="time">' + ora.getHours() + ':' + ora.getMinutes() + '</span>' + '</div>' + '<div class="clearfix">' + '</div>'
         );
         $(this).val('');
         window.setTimeout(function(){
-          $('.rightcontainer').append(
+          $('.messagescontainer').append(
             '<div class="messages leftwhite">' + 'Ok!' + '<span class="time">' + ora.getHours() + ':' + ora.getMinutes() + '</span>' + '</div>' + '<div class="clearfix">' + '</div>'
           );
         },1000);
@@ -19,17 +19,21 @@ $(document).ready(function(){
     }
   });
 
-  $('#cerca').keypress(function(e){
-    if(e.which==13){
+  $('#cerca').keyup(function(){
+
       var researched = $('#cerca').val()
       for (var i = 1; i < 7; i++) {
         var string = $('#' + i).html();
         if (string.search(researched)>=0) {
-          $('#' + i).parent().parent().removeClass('noresult');
+          $('#' + i).parent().parent().show();
+        }
+        else {
+          $('#' + i).parent().parent().hide();
         }
       }
-      $('.noresult').hide();
-    }
+
   });
+
+  $('.friendstripe')
 
 });
